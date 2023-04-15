c07fc22f9824294e807178579069cda82f843639
Neelanjan Chakraborty <neelanjanchakraborty231@gmail.com>
2023-04-15 22:36:50 +0530

    some minor weaks

--- TempConv.py
+++ TempConv.py
@@ -1,60 +1,61 @@
 import tkinter as tk
-import tkinter.messagebox as messagebox
-import tkinter.ttk as ttk
+from tkinter import ttk, messagebox
 import pyperclip
 
+
 class TemperatureConverter(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master):
         super().__init__(master)
 
-        # Set the window title
+        self.master = master
         self.master.title("Temperature Converter")
-
-        # Set the window size
-        self.master.geometry("300x250")
-
-        # Create a style for the frame and widgets
-        self.style = ttk.Style(self)
+        self.style = ttk.Style()
         self.style.theme_use("default")
-        self.style.configure("TFrame", background="#f2f2f2")
-        self.style.configure("TLabel", background="#f2f2f2")
-        self.style.configure("TRadiobutton", background="#f2f2f2")
-        self.style.configure("TButton", background="#f2f2f2")
-        self.style.configure("TEntry", background="#fff")
+        self.create_widgets()
 
-        # Create the widgets
-        self.temperature_label = ttk.Label(self, text="Temperature:")
-        self.temperature_entry = ttk.Entry(self, width=8)
-        self.celsius_button = ttk.Radiobutton(self, text="Celsius", variable=self.scale_var, value="Celsius")
-        self.fahrenheit_button = ttk.Radiobutton(self, text="Fahrenheit", variable=self.scale_var, value="Fahrenheit")
-        self.kelvin_button = ttk.Radiobutton(self, text="Kelvin", variable=self.scale_var, value="Kelvin")
-        self.convert_button = ttk.Button(self, text="Convert", command=self.convert_temperature)
-        self.round_label = ttk.Label(self, text="Round to:")
-        self.round_entry = ttk.Entry(self, width=3)
-        self.round_entry.insert(tk.END, "1")
-        self.copy_button = ttk.Button(self, text="Copy to clipboard", command=self.copy_to_clipboard)
-        self.output_label = ttk.Label(self, text="", font=("Arial", 16))
+    def create_widgets(self):
+        # Create the input label and entry widget
+        self.input_label = ttk.Label(self.master, text="Enter temperature:")
+        self.input_label.grid(row=0, column=0, padx=5, pady=5)
+        self.input_entry = ttk.Entry(self.master)
+        self.input_entry.grid(row=0, column=1, padx=5, pady=5)
 
-        # Position the widgets using the grid layout manager
-        self.temperature_label.grid(row=0, column=0, padx=5, pady=5)
-        self.temperature_entry.grid(row=0, column=1, padx=5, pady=5)
-        self.celsius_button.grid(row=1, column=0, padx=5, pady=5)
-        self.fahrenheit_button.grid(row=1, column=1, padx=5, pady=5)
-        self.kelvin_button.grid(row=1, column=2, padx=5, pady=5)
-        self.convert_button.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
-        self.round_label.grid(row=3, column=0, padx=5, pady=5)
-        self.round_entry.grid(row=3, column=1, padx=5, pady=5)
-        self.copy_button.grid(row=3, column=2, padx=5, pady=5)
-        self.output_label.grid(row=4, column=0, columnspan=3, padx=5, pady=5)
+        # Create the scale label and option menu
+        self.scale_label = ttk.Label(self.master, text="Select scale:")
+        self.scale_label.grid(row=1, column=0, padx=5, pady=5)
+        self.scale_var = tk.StringVar(value="Celsius")
+        self.scale_menu = ttk.OptionMenu(self.master, self.scale_var, "Celsius", "Celsius", "Fahrenheit", "Kelvin")
+        self.scale_menu.grid(row=1, column=1, padx=5, pady=5)
 
-        # Set the focus to the temperature entry widget
-        self.temperature_entry.focus()
+        # Create the round label and entry widget
+        self.round_label = ttk.Label(self.master, text="Round to decimal places:")
+        self.round_label.grid(row=2, column=0, padx=5, pady=5)
+        self.round_entry = ttk.Entry(self.master, width=5)
+        self.round_entry.insert(tk.END, "2")
+        self.round_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        # Create the convert button
+        self.convert_button = ttk.Button(self.master, text="Convert", command=self.convert_temperature)
+        self.convert_button.grid(row=3, column=0, padx=5, pady=5)
+
+        # Create the output label and copy button
+        self.output_label = ttk.Label(self.master, text="")
+        self.output_label.grid(row=3, column=1, padx=5, pady=5)
+        self.copy_button = ttk.Button(self.master, text="Copy", command=self.copy_to_clipboard)
+        self.copy_button.grid(row=4, column=1, padx=5, pady=5)
+
+        # Create the dark mode button
+        self.dark_mode_button = ttk.Button(self.master, text="Dark Mode", command=self.toggle_dark_mode)
+        self.dark_mode_button.grid(row=4, column=0, padx=5, pady=5)
 
     def convert_temperature(self):
+        # Get the temperature and selected scale from the input widgets
+        temperature_string = self.input_entry.get()
+        scale = self.scale_var.get()
+
+        # Convert the temperature string to a number
         try:
-            # Get the temperature and scale from the user
-            temperature = float(self.temperature_entry.get())
-            scale = self.scale_var.get()
+            temperature = float(temperature_string)
         except ValueError:
             # Display an error message if the temperature is not a valid number
             messagebox.showerror("Error", "Invalid temperature")
@@ -71,21 +72,32 @@ class TemperatureConverter(tk.Frame):
         # Round the converted temperature to the specified number of decimal places
         try:
             decimal_places = int(self.round_entry.get())
-            converted_temperature = round(converted_temperature, decimal_places)
+            converted
         except ValueError:
-            pass
+            # Display an error message if the decimal places is not a valid integer
+            messagebox.showerror("Error", "Invalid number of decimal places")
+            return
 
-        # Display the converted temperature
-        self.output_label.configure(text=f"{converted_temperature:.2f} {scale}")
+        rounded_temperature = round(converted_temperature, decimal_places)
+
+        # Update the output label with the converted temperature
+        self.output_label.config(text=f"{rounded_temperature:.{decimal_places}f} {scale}")
 
     def copy_to_clipboard(self):
         # Copy the converted temperature to the clipboard
         converted_temperature = self.output_label.cget("text")
         pyperclip.copy(converted_temperature)
-        messagebox.showinfo("Information", "Temperature copied to clipboard")
 
-if name == "main":
+    def toggle_dark_mode(self):
+        # Toggle dark mode
+        if self.style.theme_use() == "default":
+            self.style.theme_use("clam")
+        else:
+            self.style.theme_use("default")
+
+
+if __name__ == "__main__":
     root = tk.Tk()
     app = TemperatureConverter(root)
-    app.mainloop()
-
+    #app.pack()
+    root.mainloop()
